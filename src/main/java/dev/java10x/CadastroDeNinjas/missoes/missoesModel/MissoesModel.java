01474dcdbf3c6142f05d1bb05888f1053b399519
Lucas Emanuel <fit.luk@outlook.com>
2025-07-27 15:52:57 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/missoesModel/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/missoesModel/MissoesModel.java
@@ -2,10 +2,17 @@ package dev.java10x.CadastroDeNinjas.missoes.missoesModel;
 
 import dev.java10x.CadastroDeNinjas.ninja.ninjaModel.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
+
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/ninja/ninjaModel/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninja/ninjaModel/NinjaModel.java
@@ -2,9 +2,16 @@ package dev.java10x.CadastroDeNinjas.ninja.ninjaModel;
 
 import dev.java10x.CadastroDeNinjas.missoes.missoesModel.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity //cria uma entidade para o nosso banco de dados
 @Table(name = "tb_cadastro") //da o nome da nossa tabela
+@NoArgsConstructor //cria construtores automaticamente com o uso do lombok
+@AllArgsConstructor
+@Data //cria os getters e setters com o uso do lombok
 public class NinjaModel {
 
     @Id //marca minha variavel como um id
@@ -21,45 +28,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //juntando uma tabela com outra com o nome missoes_id
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
