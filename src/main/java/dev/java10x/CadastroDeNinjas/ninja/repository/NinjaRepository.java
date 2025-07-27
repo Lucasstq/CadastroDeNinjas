adb8998b81fe3195c21ff6bb426e456d5b98c151
Lucas Emanuel <fit.luk@outlook.com>
2025-07-27 16:00:22 -0300

    feat: Cria os arquivos de Service e Repository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninja/repository/NinjaRepository.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.ninja.repository;
+
+public interface NinjaRepository {
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninja/services/NinjaServices.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.ninja.services;
+
+public class NinjaServices {
+}
