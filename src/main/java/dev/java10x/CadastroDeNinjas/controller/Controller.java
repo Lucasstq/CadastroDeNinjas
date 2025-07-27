f606ceb8b021e64f2fd93df4620a63cbc47ef699
Lucas Emanuel <fit.luk@outlook.com>
2025-07-27 15:24:15 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //marcar classes como controladores Rest
+@RequestMapping //mapear URLs específicas para métodos dentro dos controladores
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasvindas() {
+        return "Cadastro de Ninjas";
+    }
+
+}
