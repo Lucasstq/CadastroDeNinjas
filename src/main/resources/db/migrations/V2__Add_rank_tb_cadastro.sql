2a3fd66ad83e118e80a742c7349d6e473ba57fcd
Lucas Emanuel <fit.luk@outlook.com>
2025-07-28 14:28:18 -0300

    fix: corrige o erro de escrita do arquivo migrations no application

--- src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
+++ src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
@@ -1,6 +1,6 @@
 --Migration para adicionar coluna rank na minha tabela de cadastro
 
-ALTER TABLE tb_cadastro
-ADD COLUMN Rank VARCHAR(50);
+ALTER TABLE TB_CADASTRO
+ADD COLUMN RANKING VARCHAR(50);
 
 --Não posso criar mais colunas aqui, para criar uma nova coluna precisamos criar uma nova versão como V3__
